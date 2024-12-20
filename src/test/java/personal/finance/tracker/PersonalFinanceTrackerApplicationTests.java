6fad07e841b650bf66637cba39f0ea20fb49f30f
Shubham Kumar <34638812+ishubhamkr@users.noreply.github.com>
2024-12-20 13:40:13 +0530

    Project Creation

---
+++ src/test/java/personal/finance/tracker/PersonalFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.finance.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
